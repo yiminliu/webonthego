1746475d06f30cdfb36cceefe91df5140eca2f74
yiminliu <yimin_l@hotmail.com>
2013-03-05 09:39:00 -0800

    added logic to refresh cache of payment history after refund

--- refund/com/tscp/mvna/domain/controller/RefundController.java
+++ refund/com/tscp/mvna/domain/controller/RefundController.java
@@ -14,26 +14,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-//import com.trc.domain.refund.RefundRequest;
+import com.trc.exception.management.AccountManagementException;
 import com.trc.exception.management.PaymentManagementException;
 import com.trc.exception.management.RefundManagementException;
-//import com.trc.manager.PaymentManager;
+import com.trc.manager.AccountManager;
 import com.trc.manager.PaymentManager;
 import com.trc.manager.RefundManager;
 import com.trc.manager.UserManager;
-//import com.trc.manager.impl.PaymentManager;
-//import com.trc.manager.impl.PaymentManager;
-//import com.trc.manager.impl.UserManager;
 import com.trc.domain.refund.RefundCode;
 import com.trc.domain.refund.RefundRequest;
-//import com.trc.payment.refund.RefundRequest;
 import com.trc.user.User;
+import com.trc.user.account.PaymentHistory;
 import com.trc.web.model.ResultModel;
+import com.trc.web.session.cache.CacheKey;
+import com.trc.web.session.cache.CacheManager;
+import com.trc.web.validation.JCaptchaValidator;
 import com.trc.web.validation.RefundRequestValidator;
-//import com.trc.web.session.SessionRequest;
-//import com.trc.web.session.SessionToken;
-//import com.trc.web.validation.JcaptchaValidator;
-//import com.trc.web.validation.RefundRequestValidator;
 import com.tscp.mvne.PaymentTransaction;
 import com.tscp.util.logger.DevLogger;
 
@@ -48,6 +44,8 @@ public class RefundController {
   private RefundManager refundManager;
   @Autowired
   private RefundRequestValidator refundRequestValidator;
+  @Autowired
+  private AccountManager accountManager;
 
   @ModelAttribute
   protected void refundReferenceData(ModelMap modelMap) {
@@ -72,23 +70,27 @@ public class RefundController {
 
   @RequestMapping(value = "{transId}", method = RequestMethod.POST)
   public ModelAndView processRefund(HttpServletRequest request, @ModelAttribute RefundRequest refundRequest, BindingResult result) {
-    ResultModel resultModel = new ResultModel("redirect:/account/payment/history", "/account/payment/refund/confirm");
-    User user = userManager.getCurrentUser();
-    //SessionToken token = SessionManager.fetchToken(refundRequest.getSessionToken().getRequest());
-    //if (token != null && token.getId().equals(refundRequest.getSessionToken().getId())) {
-    //  JcaptchaValidator.validate(request, result);
+      ResultModel resultModel = new ResultModel("redirect:/account/payment/history", "/account/payment/refund/confirm");
+      User user = userManager.getCurrentUser();
+      //SessionToken token = SessionManager.fetchToken(refundRequest.getSessionToken().getRequest());
+      //if (token != null && token.getId().equals(refundRequest.getSessionToken().getId())) {
+      JCaptchaValidator.validate(request, result);
       refundRequestValidator.validate(refundRequest, result);
       if (result.hasErrors()) {
-        DevLogger.log("refundController.processRefund() has errors " + result.getAllErrors().toString());
-        return resultModel.getError();
+         DevLogger.log("refundController.processRefund() has errors " + result.getAllErrors().toString());
+         return resultModel.getError();
       } else {
         //token.consume();
-        try {
-          refundManager.refundPayment(user, refundRequest);
-        } catch (RefundManagementException e) {
-          return resultModel.getAccessException();
-        }
-        return resultModel.getSuccess();
+         try {
+           refundManager.refundPayment(user, refundRequest);
+           PaymentHistory paymentHistory = new PaymentHistory(accountManager.getPaymentRecords(user), user);
+           new CacheManager().set(CacheKey.PAYMENT_HISTORY, paymentHistory);               
+         } catch (RefundManagementException e) {
+        	 return resultModel.getAccessException();
+         } catch (AccountManagementException e) {
+      	     return resultModel.getAccessException();
+         }
+         return resultModel.getSuccess();
       }
     //} else {
       //return resultModel.getAccessException();
