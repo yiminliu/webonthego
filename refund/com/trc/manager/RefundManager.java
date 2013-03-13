d1a4d7872d0f83f6b8c8db6e8cf5fdd469bedc34
yiminliu <yimin_l@hotmail.com>
2013-03-13 12:17:32 -0700

    added isRefundable() method

--- refund/com/trc/manager/RefundManager.java
+++ refund/com/trc/manager/RefundManager.java
@@ -7,30 +7,41 @@ import org.springframework.stereotype.Component;
 
 import com.trc.domain.refund.RefundCode;
 import com.trc.domain.refund.RefundRequest;
+import com.trc.exception.management.PaymentManagementException;
 import com.trc.exception.management.RefundManagementException;
 import com.trc.exception.service.RefundServiceException;
 import com.trc.service.RefundService;
 import com.trc.user.User;
 
+import com.tscp.mvne.CreditCard;
+import com.tscp.mvne.PaymentTransaction;
 import com.tscp.util.logger.LogLevel;
 import com.tscp.util.logger.aspect.Loggable;
 
 @Component
 public class RefundManager implements RefundManagerModel {
   @Autowired
-  private RefundService refundService;
+  private RefundService refundService;  
+  @Autowired
+  private PaymentManager paymentManager;
         
   @PreAuthorize("isAuthenticated() and hasPermission(#user, 'canRefund')")
-  public void refundPayment(User user, RefundRequest paymentRefund, int transId) throws RefundManagementException {
-	 synchronized(paymentRefund.getPaymentTransaction()) {
-        refundPayment(paymentRefund.getPaymentTransaction().getAccountNo(), 
-        transId, 
-        paymentRefund.getPaymentTransaction().getPaymentAmount(), 
-        String.valueOf(paymentRefund.getPaymentTransaction().getBillingTrackingId()), 
-    	user, 
-    	paymentRefund.getRefundCode(), 
-    	paymentRefund.getNotes());
-	 }   
+  public void refundPayment(User user, RefundRequest refundRequest, int transId) throws RefundManagementException {
+	 try{
+	    if(isRefundable(refundRequest.getPaymentTransaction())) { 
+	       synchronized(refundRequest.getPaymentTransaction()) {
+              refundPayment(refundRequest.getPaymentTransaction().getAccountNo(), 
+              transId, 
+              refundRequest.getPaymentTransaction().getPaymentAmount(), 
+              String.valueOf(refundRequest.getPaymentTransaction().getBillingTrackingId()), 
+    	      user, 
+    	      refundRequest.getRefundCode(), 
+    	      refundRequest.getNotes());
+	      }  
+	    } 
+	 } catch(Exception e){
+		 throw new RefundManagementException("Refund failed due to: " + e.getMessage());
+	 }
   }
   
   @Loggable(value = LogLevel.TRACE)
@@ -41,6 +52,22 @@ public class RefundManager implements RefundManagerModel {
      catch (RefundServiceException e) {
         throw new RefundManagementException(e.getMessage(), e.getCause());
      }
+  }
+  
+  public boolean isRefundable(PaymentTransaction paymentTransaction) throws RefundManagementException {
+	  CreditCard creditCard = null;
+	  try{
+		  paymentManager.getCreditCard(paymentTransaction.getPmtId());
+	  } catch(PaymentManagementException pe){
+		  throw new RefundManagementException("Unable to refund because error occured while getting credit card information: " + pe.getMessage());
+ 	  }
+	  if (paymentTransaction.getTransId() < 0)
+	  	  throw new RefundManagementException("Unable to refund because no valid Transaction ID found.");
+	  
+	  if (paymentTransaction.getBillingTrackingId() < 0)
+		  throw new RefundManagementException("Unable to refund because no valid Billing Tracking ID found.");
+	  
+	  return creditCard != null;
   }
 
   
