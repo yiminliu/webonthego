d840a99f7139913e48e871959999feec730d553b
yiminliu <yimin_l@hotmail.com>
2013-03-13 10:51:10 -0700

    removed a method

--- refund/com/trc/manager/RefundManagerModel.java
+++ refund/com/trc/manager/RefundManagerModel.java
@@ -1,12 +1,11 @@
 package com.trc.manager;
 
-import com.trc.domain.refund.RefundCode;
 import com.trc.domain.refund.RefundRequest;
 import com.trc.exception.management.RefundManagementException;
 import com.trc.user.User;
 
 public interface RefundManagerModel {
-	 public void refundPayment(int accountNo, int transId, String amount, String trackingId, User user, RefundCode refundCode, String notes) throws RefundManagementException;
+	 //public void refundPayment(int accountNo, int transId, String amount, String trackingId, User user, RefundCode refundCode, String notes) throws RefundManagementException;
 
 	 public void refundPayment(User user, RefundRequest paymentRefund, int transId) throws RefundManagementException;
  
